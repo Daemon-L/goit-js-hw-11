9db7ca7b6efa3c960a42c963f40aa884e3966a5d
Daemon-L <lit.dmitriy.ua@gmail.com>
2021-11-19 22:12:21 +0200

    Добавляет файлы, зависимости, библиотеки

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-import './sass/main.scss';
+import './css/styles.css';
+import debounce from 'lodash.debounce';
+import axios from 'axios';
+import Notiflix from 'notiflix';
+
+import {fetchImages} from "./js/fetchImages";
+
+// const DEBOUNCE_DELAY = 300;
+
+// const refs = {
+//     countryInput: document.querySelector("#search-box"),
+//     countryList: document.querySelector('.country-list'),
+//     countryInfo: document.querySelector('.country-info'),
+// }
+
+
+// Notiflix.Notify.info("Too many matches found. Please enter a more specific name.")
+// Notiflix.Notify.failure("Oops, there is no country with that name");
