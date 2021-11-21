db7b50045d23a82c17e8926d6d6cbc129e769d0c
Daemon-L <lit.dmitriy.ua@gmail.com>
2021-11-21 21:39:54 +0200

    Выполняет задание ДЗ

--- src/index.js
+++ src/index.js
@@ -1,18 +1,100 @@
 import './css/styles.css';
-import debounce from 'lodash.debounce';
-import axios from 'axios';
 import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { fetchImages } from "./js/fetchImages";
 
-import {fetchImages} from "./js/fetchImages";
+const refs = {
+  form: document.querySelector('#search-form'),
+  gallery: document.querySelector('.gallery'),
+  loadMoreButton: document.querySelector('.load-more'),
+};
 
-// const DEBOUNCE_DELAY = 300;
+let pictureName = '';
+hideButton(refs.loadMoreButton);
 
-// const refs = {
-//     countryInput: document.querySelector("#search-box"),
-//     countryList: document.querySelector('.country-list'),
-//     countryInfo: document.querySelector('.country-info'),
-// }
+refs.form.addEventListener('submit', Search);
+refs.loadMoreButton.addEventListener('click', LoadMore);
 
+function Search(e) {
+  e.preventDefault();
+  pictureName = e.currentTarget.searchQuery.value;
 
-// Notiflix.Notify.info("Too many matches found. Please enter a more specific name.")
-// Notiflix.Notify.failure("Oops, there is no country with that name");
+  hideButton(refs.loadMoreButton);
+  fetchImages(pictureName).then(images => {
+    const imagesArray = images.data.hits;
+    const totalImages = images.data.totalHits;
+
+    if (imagesArray.length === 0) {
+      return Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.',);
+    } else {
+      clear();
+      markupGallery(imagesArray);
+      new SimpleLightbox('.gallery a', { captionDelay: 250, showCounter: false });
+      Notiflix.Notify.success(`Hooray! We found ${totalImages} images.`);
+      showButton(refs.loadMoreButton);
+    }
+  });
+}
+
+function LoadMore() {
+  fetchImages(pictureName)
+    .then(images => {
+      const imagesArray = images.data.hits;
+      if (imagesArray.length === 0) {
+        Notiflix.Notify.failure("We're sorry, but you've reached the end of search results.");
+        hideButton(refs.loadMoreButton);
+        return;
+      }
+      markupGallery(imagesArray);
+      new SimpleLightbox('.gallery a', { captionDelay: 250, showCounter: false });
+    })
+    .catch(error => {
+      console.log(error);
+      Notiflix.Notify.failure("We're sorry, but you've reached the end of search results.");
+      hideButton(refs.loadMoreButton);
+    });
+}
+
+function markupGallery(images) {
+  const markup = images
+    .map(image => {
+      return `
+        <a class="gallery__item" href="${image.largeImageURL}">
+          <div class="photo-card">
+            <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
+            <div class="info">
+              <p class="info-item">
+                <b>Likes</b>
+                ${image.likes}
+              </p>
+              <p class="info-item">
+                <b>Views</b>
+                ${image.views}
+              </p>
+              <p class="info-item">
+                <b>Comments</b>
+                ${image.comments}
+              </p>
+              <p class="info-item">
+                <b>Downloads</b>
+                ${image.downloads}
+              </p>
+            </div>
+          </div>
+        </a>`;
+    })
+    .join('');
+    refs.gallery.insertAdjacentHTML('beforeend', markup);
+}
+
+function clear() {
+  refs.gallery.innerHTML = '';
+}
+
+function hideButton(item) {
+    item.classList.add('visually-hidden');
+}
+function showButton(item) {
+    item.classList.remove('visually-hidden');
+}
---
+++ src/js/fetchImages.js
@@ -0,0 +1,48 @@
+import axios from 'axios';
+export { fetchImages };
+    
+const BASE_URL = 'https://pixabay.com/api/';
+const API_KEY = `24425918-1c58292ec38f4df582c31de2d`;
+let page = 1;
+
+async function fetchImages(pictureName) {
+  const FULL_URL =
+    `${BASE_URL}?key=${API_KEY}&q=${pictureName}
+    &image_type=photo&orientation=horizontal&safesearch=true&page=${page}
+    &per_page=${40}`;
+  
+  try {
+    const images = await axios.get(FULL_URL);
+    page += 1;
+    // console.log(images);     
+    return await images;  
+  }
+  catch (error) {
+    console.log(error);
+  }
+}
+
+
+// async function fetchImages(pictureName) {
+//   try {
+//     const response = await axios.get(
+//       `${BASE_URL}?key=${API_KEY}&q=${pictureName}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`,
+//     );
+//     // page += 1;
+//     //     return await response;
+//   }
+//   catch (error) {
+//     console.log(error);
+//   }
+// }
+
+
+// var URL = "https://pixabay.com/api/?key="+API_KEY+"&q="+encodeURIComponent('red roses');
+// $.getJSON(URL, function(data){
+// if (parseInt(data.totalHits) > 0)
+//     $.each(data.hits, function(i, hit){ console.log(hit.pageURL); });
+// else
+//     console.log('No hits');
+// });
+
+// Ваш ключ API: 24425918-1c58292ec38f4df582c31de2d
